d71e81b6581bc5c6377a6af721f0b6853c128ba0
bkrmadtya <karki.bikram007@gmail.com>
2020-07-08 18:55:30 +0300

    added flex style to the BarContainer

--- src/components/BarContainer.js
+++ src/components/BarContainer.js
@@ -1,12 +1,23 @@
 import React from "react";
 
+const style = {
+  position: "absolute",
+  bottom: "40%",
+  left: 0,
+  right: 0,
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "flex-end",
+};
+
 const BarContainer = ({ array }) => {
   return (
-    <>
+    <div style={style}>
       {array.map((value) => (
         <div key={value}>{value}</div>
       ))}
-    </>
+    </div>
   );
 };
 
