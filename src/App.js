86bb2470c56a0e9185d6c181427aa04c4e12a663
bkrmadtya <karki.bikram007@gmail.com>
2020-07-09 14:50:00 +0300

    renamed BarContainer to SortingVisualizer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 
-import BarContainer from "./components/BarContainer";
+import BarContainer from "./components/SortingVisualizer";
 
 function App() {
-  const [array, setArray] = useState([100, 200, 300]);
   return (
     <div>
       <h1>Sorting Algorithm Visualizer</h1>
