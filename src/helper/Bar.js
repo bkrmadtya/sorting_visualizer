1b7d0ca22e8d9b97604953b4f33058a7c4d7aef7
bkrmadtya <karki.bikram007@gmail.com>
2020-07-10 23:28:59 +0300

    added documentation comment for the Bar class

--- src/helper/Bar.js
+++ src/helper/Bar.js
@@ -1,3 +1,9 @@
+/**
+ * This class represents an element or bar of the array
+ * @property height - height of the element
+ * @property state - state of the element i.e. [UNSORTED, ACTIVE, UNSWAPPED, SWAPPED, SORTED]
+ */
+
 import colors from "helper/colors";
 import states from "helper/states";
 
